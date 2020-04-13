403762ce322fd2995e4a550b774f2e4638ae7ed8
Asia95 <janaszek@onet.pl>
2020-04-13 22:43:39 +0100

    lab2 - part 1

--- lab2/lab2_part1.py
+++ lab2/lab2_part1.py
@@ -43,6 +43,88 @@ model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.2),
               metrics=['accuracy'])
 
 BATCH_SIZE = 64
-EPOCHS = 7
+EPOCHS = 10
 
 model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
+
+'''TODO: Use the evaluate method to test the model!'''
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+
+def build_cnn_model():
+    cnn_model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(filters=24, kernel_size=(3, 3), activation=tf.nn.relu),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(filters=36, kernel_size=(3, 3), activation=tf.nn.relu),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    return cnn_model
+
+
+cnn_model = build_cnn_model()
+# Initialize the model by passing some data through
+cnn_model.predict(train_images[[0]])
+# Print the summary of the layers in the model.
+print(cnn_model.summary())
+
+'''TODO: Define the compile operation with your optimizer and learning rate of choice'''
+cnn_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+'''TODO: Use model.fit to train the CNN model, with the same batch_size and number of epochs previously used.'''
+cnn_model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
+
+'''TODO: Use the evaluate method to test the model!'''
+test_loss, test_acc = cnn_model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+predictions = cnn_model.predict(test_images)
+prediction = np.argmax(predictions[0])
+print(prediction)
+print("Label of this digit is:", test_labels[0])
+plt.imshow(test_images[0,:,:,0], cmap=plt.cm.binary)
+
+# Plots the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 4
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  mdl.lab2.plot_image_prediction(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  mdl.lab2.plot_value_prediction(i, predictions, test_labels)
+
+cnn_model = build_cnn_model()
+
+batch_size = 12
+loss_history = mdl.util.LossHistory(smoothing_factor=0.95) # to record the evolution of the loss
+plotter = mdl.util.PeriodicPlotter(sec=2, xlabel='Iterations', ylabel='Loss', scale='semilogy')
+optimizer = tf.keras.optimizers.SGD(learning_rate=1e-2) # define our optimizer
+
+if hasattr(tqdm, '_instances'): tqdm._instances.clear() # clear if it exists
+
+for idx in tqdm(range(0, train_images.shape[0], batch_size)):
+  # First grab a batch of training data and convert the input images to tensors
+  (images, labels) = (train_images[idx:idx+batch_size], train_labels[idx:idx+batch_size])
+  images = tf.convert_to_tensor(images, dtype=tf.float32)
+
+  # GradientTape to record differentiation operations
+  with tf.GradientTape() as tape:
+    logits = cnn_model(images)
+    loss_value = tf.keras.backend.sparse_categorical_crossentropy(labels, logits)
+
+  loss_history.append(loss_value.numpy().mean()) # append the loss to the loss_history record
+  plotter.plot(loss_history.get())
+
+  # Backpropagation
+  '''TODO: Use the tape to compute the gradient against all parameters in the CNN model.
+      Use cnn_model.trainable_variables to access these parameters.'''
+  grads = tape.gradient(loss_value, cnn_model.trainable_variables)
+  optimizer.apply_gradients(zip(grads, cnn_model.trainable_variables))
